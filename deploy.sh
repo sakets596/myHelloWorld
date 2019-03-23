b80d98639b0ce35439b8ddc68288e42054e299e6
root <root@instance-1.us-east1-b.c.windy-art-235009.internal>
2019-03-23 17:23:40 +0000

    adding mask password

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 if [ $ENVIRONMENT = "PROD" ]; then
    echo Deploying to "$ENVIRONMENT" Environment ....
-   sshpass -p "prod_tomcat" scp target/myHelloWorld.war tomcat1:/packages/apache-tomcat-9.0.16/webapps
+   sshpass -p $prod_passwd scp target/myHelloWorld.war tomcat1:/packages/apache-tomcat-9.0.16/webapps
 elif [ $ENVIRONMENT = "DEV" ]; then
    echo Deploying to "$ENVIRONMENT" Environment....
    sshpass -p "dev_tomcat" scp target/myHelloWorld.war tomcat2:/packages/apache-tomcat-9.0.16/webapps

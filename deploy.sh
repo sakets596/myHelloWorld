315d543e1a8077d1843bc0c939d76b7c39b9404e
root <root@instance-1.us-east1-b.c.windy-art-235009.internal>
2019-03-23 16:50:15 +0000

    rectifying deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ $ENVIRONMENT = "PROD" ]
+if [ $1 = "PROD" ]
 then
 sshpass -p $prod_passwd scp /target/HelloWorld.war tomcat1:/packages/apache-tomcat-9.0.16/webapps
-else [ $ENVIRONMENT = "DEV" ]
+else [ $1 = "DEV" ]
 sshpass -p $dev_passwd scp /target/HelloWorld.war tomcat2:/packages/apache-tomcat-9.0.16/webapps
 fi
